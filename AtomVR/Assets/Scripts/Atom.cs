1af576ad11b5ced9ab7db9217971f37897a50076
Henrik <henha18@student.sdu.dk>
2020-11-18 15:26:24 +0100

    Udvidelse af Atom struktur

--- AtomVR/Assets/Scripts/Atom.cs
+++ AtomVR/Assets/Scripts/Atom.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Atom : MonoBehaviour
 {
-    private List<Proton> Protons;
-    private List<Neutron> Neutrons;
-    private List<Electron> Electrons;
+    public List<GameObject> Protons;
+    public List<GameObject> Neutrons;
+    public List<GameObject> Electrons;
     
     public float rotationTimer = 0.5f;
     private float rotationCoords;
@@ -21,5 +21,33 @@ public class Atom : MonoBehaviour
             rotationTimer = Time.time + 1f;
         }
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 3f * Time.deltaTime);
+    }
+
+    void Start()
+    {
+        AtomUpdate();
+    }
+
+    void AtomUpdate()
+    {
+        int children = transform.childCount;
+
+        for (int i = 0; i < children; ++i)
+        {
+            GameObject particle = transform.GetChild(i).gameObject;
+
+            if (particle.GetComponent<Proton>())
+            {
+                Protons.Add(particle);
+            }
+            else if (particle.GetComponent<Neutron>())
+            {
+                Neutrons.Add(particle);
+            }
+            else if (particle.GetComponent<Electron>())
+            {
+                Electrons.Add(particle);
+            }
+        }
     }
 }
